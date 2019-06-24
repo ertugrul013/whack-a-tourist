5feeea30f4202d5f668fe211c6948e4aff37c6c1
SanneWouters <sannewouters99@Hotmail.com>
2019-06-24 15:21:24 +0200

    GooiSysteem

---
+++ Assets/Scripts/cameraMovement.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class cameraMovement : MonoBehaviour
+{
+    float speed = 1f;
+
+
+    void Update()
+    { 
+        transform.Rotate(-Input.GetAxis("Mouse Y"),0,0 * -Time.deltaTime * speed);
+
+        //if (transform.position.x < 2 && transform.position.x > -2)
+        //{
+        //    this.transform.Translate(Input.GetAxis("Horizontal"), 0, 0);
+        //}
+
+        float xMove = Input.GetAxis("Horizontal") * Time.deltaTime * 10;
+
+        transform.Translate(xMove,0f, 0f);
+   
+        Vector3 clampedPosition = transform.position;
+  
+        clampedPosition.x = Mathf.Clamp(clampedPosition.x, -2f, 2f);
+
+        transform.position = clampedPosition;
+
+
+
+
+    }
+}
