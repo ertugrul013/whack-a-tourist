b27d7e157fbfa4b1a8cea8fc8a51213a989ab3ea
SanneWouters <sannewouters99@Hotmail.com>
2019-06-25 11:54:28 +0200

    UpdateMovementUpDown

--- Assets/Scripts/playerMovement.cs
+++ Assets/Scripts/playerMovement.cs
@@ -15,7 +15,7 @@ public class playerMovement : MonoBehaviour
         {
           
           //  if(Input.GetKeyUp(KeyCode.Space))
-            if (Input.GetMouseButtonDown(0))
+            if ((Input.GetMouseButtonDown(0)) || Input.GetKeyUp(KeyCode.Space))
             {
                 Instantiate(coco, Camera.main.transform.position + (transform.forward * 2), Camera.main.transform.rotation);
                 aantalCoco--;
